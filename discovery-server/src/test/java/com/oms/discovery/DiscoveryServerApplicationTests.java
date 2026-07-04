dc3d21ae6da7038f97cc47ac94ace36bfc990da6
Puneet <puneet@example.com>
2026-07-04 22:34:27 +0530

    test(discovery-server): add context load unit test and test dependencies

---
+++ discovery-server/src/test/java/com/oms/discovery/DiscoveryServerApplicationTests.java
@@ -0,0 +1,14 @@
+package com.oms.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        // Simple smoke-test that the Spring application context starts successfully
+    }
+}
+
